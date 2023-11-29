fe0fc05717fd7b5b8f79b5363fa71b67e907cc2a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-29 00:00:48 -0300

    [:gear:] | setup: config - database (test/resources)

--- src/main/java/com/ms/user/UserApplication.java
+++ src/main/java/com/ms/user/UserApplication.java
@@ -2,12 +2,20 @@ package com.ms.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public ResponseEntity<String> hello(){
+		return ResponseEntity.ok("{\"message\": \"Hello World\"}");
+	}
 }
