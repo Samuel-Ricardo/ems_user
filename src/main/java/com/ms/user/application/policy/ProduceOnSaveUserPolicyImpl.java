19053cf354aa1590345b1cf59b74ef51e6036219
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-29 23:39:53 -0300

    [:scroll:] | implements: policy - produce [on] > save [user] (domain)

--- src/main/java/com/ms/user/application/policy/ProduceOnSaveUserPolicyImpl.java
+++ src/main/java/com/ms/user/application/policy/ProduceOnSaveUserPolicyImpl.java
@@ -2,12 +2,20 @@ package com.ms.user.application.policy;
 
 import com.ms.user.domain.entity.User;
 import com.ms.user.domain.policy.ProduceOnSaveUserPolicy;
+import com.ms.user.domain.producer.UserProducer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Callable;
+
 @Component
+@RequiredArgsConstructor
 public class ProduceOnSaveUserPolicyImpl implements ProduceOnSaveUserPolicy {
+
+    final UserProducer producer;
+
     @Override
-    public void execute(User user) {
-        
+    public void apply(Callable<User> save) throws Exception {
+        producer.publishMessageEmail(save.call());
     }
 }
--- src/main/java/com/ms/user/domain/policy/ProduceOnSaveUserPolicy.java
+++ src/main/java/com/ms/user/domain/policy/ProduceOnSaveUserPolicy.java
@@ -2,6 +2,9 @@ package com.ms.user.domain.policy;
 
 import com.ms.user.domain.entity.User;
 
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
 public interface ProduceOnSaveUserPolicy {
-    public void execute(User user);
+    public void apply(Callable<User> save) throws Exception;
 }
