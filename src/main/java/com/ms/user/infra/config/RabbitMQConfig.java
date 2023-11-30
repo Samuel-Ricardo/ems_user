cb937f4a2af303ea6964e1293c5808229ae9b9a5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-29 22:32:22 -0300

    [:gear:] | create: config - RabbitMQ (infra)

---
+++ src/main/java/com/ms/user/infra/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.ms.user.infra.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter(new ObjectMapper());
+    }
+
+}
