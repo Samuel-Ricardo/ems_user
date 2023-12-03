f37c757dd2a88652b0a3e88695d98c817132919a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:24:30 -0300

    [:passport_control:] | setup: service - user (application)

---
+++ src/main/java/com/ms/user/application/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ms.user.application.service;
+
+import com.ms.user.domain.entity.User;
+import com.ms.user.domain.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+}
