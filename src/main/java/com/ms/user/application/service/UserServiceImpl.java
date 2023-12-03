de55374e6d319c93c8d068df6cec9ea879c18fdd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:32:53 -0300

    [:sparkles:] | implements: service - user (application)

--- src/main/java/com/ms/user/application/service/UserServiceImpl.java
+++ src/main/java/com/ms/user/application/service/UserServiceImpl.java
@@ -1,14 +1,22 @@
 package com.ms.user.application.service;
 
+import com.ms.user.application.usecase.SaveUserUseCase;
 import com.ms.user.domain.entity.User;
+import com.ms.user.domain.policy.ProduceOnSaveUserPolicy;
 import com.ms.user.domain.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final ProduceOnSaveUserPolicy produceOnSaveUserPolicy;
+    private final SaveUserUseCase save;
+
     @Override
-    public User save(User user) {
-        return null;
+    public User save(User user) throws Exception {
+        return produceOnSaveUserPolicy.apply(() -> save.execute(user));
     }
 }
--- src/main/java/com/ms/user/application/usecase/SaveUserUseCase.java
+++ src/main/java/com/ms/user/application/usecase/SaveUserUseCase.java
@@ -2,11 +2,13 @@ package com.ms.user.application.usecase;
 
 import com.ms.user.domain.entity.User;
 import com.ms.user.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SaveUserUseCase {
 
+    @Autowired
     private UserRepository repository;
 
     public User execute(User user) {
