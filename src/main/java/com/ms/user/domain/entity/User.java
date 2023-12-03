cc41500c3cf8409e876f41f4a9005efb939e3020
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 15:03:21 -0300

    [:sparkles:] | create: entity - user > of (domain)

--- src/main/java/com/ms/user/domain/entity/User.java
+++ src/main/java/com/ms/user/domain/entity/User.java
@@ -1,20 +1,19 @@
 package com.ms.user.domain.entity;
 
+import com.ms.user.domain.dto.UserDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.beans.BeanUtils;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
-@Entity
-@Table(name = "TB_USERS")
+@Entity @Table(name = "TB_USERS")
+@Builder @RequiredArgsConstructor
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -23,4 +22,10 @@ public class User implements Serializable {
     private UUID userId;
     private String name;
     private String email;
+
+    public static User of(UserDTO dto) {
+        var user = new User();
+        BeanUtils.copyProperties(user, dto);
+        return user;
+    }
 }
