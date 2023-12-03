511d9cc974c2877c0f73a209e8ba8b6b2df05a23
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:42:24 -0300

    [:sparkles:] | update: service - user > get [all] (application)

--- src/main/java/com/ms/user/application/service/UserServiceImpl.java
+++ src/main/java/com/ms/user/application/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ms.user.application.service;
 
+import com.ms.user.application.usecase.GetAllUsersUseCase;
 import com.ms.user.application.usecase.SaveUserUseCase;
 import com.ms.user.domain.entity.User;
 import com.ms.user.domain.policy.ProduceOnSaveUserPolicy;
@@ -9,15 +10,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Primary @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final ProduceOnSaveUserPolicy produceOnSaveUserPolicy;
+
     private final SaveUserUseCase save;
+    private final GetAllUsersUseCase findAll;
 
     @Override
     public User save(User user) throws Exception {
         return produceOnSaveUserPolicy.apply(() -> save.execute(user));
     }
+
+    @Override
+    public List<User> getAll() {
+        return findAll.execute();
+    }
+
+
 }
