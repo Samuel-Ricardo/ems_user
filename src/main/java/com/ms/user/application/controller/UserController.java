8817c8eaa6900c566cd17b86838b859bc8206984
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:44:26 -0300

    [:traffic_light:] | create: route - get [all] > user (application/controller)

--- src/main/java/com/ms/user/application/controller/UserController.java
+++ src/main/java/com/ms/user/application/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.ms.user.application.controller;
 
+import com.ms.user.domain.entity.User;
 import com.ms.user.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -11,5 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService service;
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(service.getAll());
+    }
 
 }
