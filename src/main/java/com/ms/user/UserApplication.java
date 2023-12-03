05fd693e916ac26071483250eda7b1d29a13ac77
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:36:45 -0300

    [:station:] | create: controller - app (application)

--- src/main/java/com/ms/user/UserApplication.java
+++ src/main/java/com/ms/user/UserApplication.java
@@ -7,15 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class UserApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public ResponseEntity<String> hello(){
-		return ResponseEntity.ok("{\"message\": \"Hello World\"}");
 	}
 }
---
+++ src/main/java/com/ms/user/application/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.ms.user.application.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("{\"message\": \"Hello World\"}");
+    }
+}
