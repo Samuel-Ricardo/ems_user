c5d472101e8b46d143137726ee641404ae93744c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:39:55 -0300

    [:sparkles:] | update: service - user > get [all] (domain)

--- src/main/java/com/ms/user/domain/service/UserService.java
+++ src/main/java/com/ms/user/domain/service/UserService.java
@@ -3,6 +3,9 @@ package com.ms.user.domain.service;
 import com.ms.user.domain.dto.UserDTO;
 import com.ms.user.domain.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     public User save (User user) throws Exception;
+    public List<User> getAll();
 }
