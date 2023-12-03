310cc3040559801732e29cd141409ea25d9b9ab9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 15:04:28 -0300

    [:traffic_light:] | create: route - save > user (application/controller)

--- src/main/java/com/ms/user/application/controller/UserController.java
+++ src/main/java/com/ms/user/application/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.ms.user.application.controller;
 
+import com.ms.user.domain.dto.UserDTO;
 import com.ms.user.domain.entity.User;
 import com.ms.user.domain.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,14 @@ public class UserController {
     @GetMapping()
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(service.getAll());
+    }
+
+    public ResponseEntity<User> saveUser(
+            @RequestBody
+            @Valid UserDTO user
+    ) throws Exception {
+        return ResponseEntity.
+                ok(service.save(User.of(user)));
     }
 
 }
