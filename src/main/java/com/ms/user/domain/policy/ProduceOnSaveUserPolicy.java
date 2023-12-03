e25b19815168fa42454994458d5558e6cd6a18d3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-03 14:30:04 -0300

    [:scroll:] | update: policy - produce > user [on save] (domain)

--- src/main/java/com/ms/user/domain/policy/ProduceOnSaveUserPolicy.java
+++ src/main/java/com/ms/user/domain/policy/ProduceOnSaveUserPolicy.java
@@ -3,8 +3,7 @@ package com.ms.user.domain.policy;
 import com.ms.user.domain.entity.User;
 
 import java.util.concurrent.Callable;
-import java.util.function.Function;
 
 public interface ProduceOnSaveUserPolicy {
-    public void apply(Callable<User> save) throws Exception;
+    public User apply(Callable<User> save) throws Exception;
 }
