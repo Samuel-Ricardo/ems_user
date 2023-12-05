1e42550fe4f7cbad52a5321b706d15ae9caeed93
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-04 22:33:15 -0300

    [:scroll:] | update: policy - produce > user [on save] (domain)

--- src/main/java/com/ms/user/application/policy/ProduceOnSaveUserPolicyImpl.java
+++ src/main/java/com/ms/user/application/policy/ProduceOnSaveUserPolicyImpl.java
@@ -4,15 +4,17 @@ import com.ms.user.domain.entity.User;
 import com.ms.user.domain.policy.ProduceOnSaveUserPolicy;
 import com.ms.user.domain.producer.UserProducer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Callable;
 
 @Component
-@RequiredArgsConstructor
 public class ProduceOnSaveUserPolicyImpl implements ProduceOnSaveUserPolicy {
 
-    final UserProducer producer;
+    @Autowired
+    private UserProducer producer;
 
     @Override
     public User apply(Callable<User> save) throws Exception {
