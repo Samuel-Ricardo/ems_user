757a987f086a68290f4f962951a3e13bf695ce7e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-04 22:32:44 -0300

    [:traffic_light:] | mapping: routes (application/controller)

--- src/main/java/com/ms/user/application/controller/UserController.java
+++ src/main/java/com/ms/user/application/controller/UserController.java
@@ -6,10 +6,7 @@ import com.ms.user.domain.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +17,12 @@ public class UserController {
 
     private final UserService service;
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(service.getAll());
     }
 
+    @PostMapping
     public ResponseEntity<User> saveUser(
             @RequestBody
             @Valid UserDTO user
